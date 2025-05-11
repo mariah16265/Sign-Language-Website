7c147a65026dbb002dd613212a4e95126999c4d0
SyedaKa <ksyeda2003@gmail.com>
2025-05-11 08:41:18 +0400

    data syncing with levels

--- BACKEND/utils/lessonGrouping.js
+++ BACKEND/utils/lessonGrouping.js
@@ -1,25 +1,41 @@
 module.exports = {
   "Arabic": {
-    "Module 1- الحروف الأبجدية": 3,
-    "Module 2- مقدمة": 2,
-    "Module 3- تحيات":2,
-    "Module 4- أيام الأسبوع":2,
-    "Module 5- الأماكن":3,
-    "Module 6- الإمارات":2
+    "Mubtadi": {
+      "Module 1- الحروف الأبجدية": 3,
+      "Module 2- مقدمة": 2,
+      "Module 3- تحيات": 2,
+      "Module 4- الأرقام": 3 
+    },
+    "Mutawassit": {
+      "Module 1- أيام الأسبوع": 2,
+      "Module 2- الأماكن": 3,
+      "Module 3- الإمارات": 2
+    }
   },
   "English": {
-    "Module 1- Alphabets":4,
-    "Module 2- Animals":2,
-    "Module 3- Colors":2,
-    "Module 4- Fruits":3,
-    "Module 5- Family":3,
-    "Module 6- Natural World":4,
-    "Module 7- Vehicles":3,
-    "Module 8- Sports":3,
-    "Module 9- Medical":4
-  },
-  "Math": {
-    "Module 1- Numbers in English(1 to 20)":4,
-    "Module 2- Numbers in Arabic(1 to 20)":3
-}
+    "Little Explorer": {
+      "Module 1- Alphabets": 3,
+      "Module 2- Colors": 2,
+      "Module 3- Greetings": 2,
+      "Module 4- Numbers": 3
+    },
+    "Word Builder": {
+      "Module 1- Animals": 2,
+      "Module 2- Fruits": 3,
+      "Module 3- Family": 3,
+      "Module 4- Natural World": 4,
+      "Module 5- Vehicles": 3,
+      "Module 6- Sports": 3,
+      "Module 7- Medical": 4
+    },
+    "Story Master": {
+      "Module 1- Animals": 2,
+      "Module 2- Fruits": 3,
+      "Module 3- Family": 3,
+      "Module 4- Natural World": 4,
+      "Module 5- Vehicles": 3,
+      "Module 6- Sports": 3,
+      "Module 7- Medical": 4
+    }
+  }
 }
